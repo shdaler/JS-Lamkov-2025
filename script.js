6c14036953ae751c5e2bd39ede13e7550ab30034
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-02-04 10:19:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ const hasMotivation = true;
 const hasFreeTime = true;
 
 if (clientName !== "Max") {
+  console.log("Please call Max");
+} else if (!hasMuchMoney || !hasGoodJob || (hasMotivation && hasFreeTime)) {
+  console.log("Max should more learn frontend");
+} else {
+  console.log(
+    "You are very rich, so you can no work forever, or you have maybe good work, or you just no need it"
+  );
 }
