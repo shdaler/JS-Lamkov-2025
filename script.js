40fe2d1f8d88cdf5ba6ef422beaaa46d72ed919b
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-02-04 09:56:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-const age = 17;
-const withParent = false;
-
-if (age > 18 || withParent) {
-  console.log("Come");
-} else {
-  console.log("age limitation");
-}
+const result = "" || null || 1;
+console.log("result");
