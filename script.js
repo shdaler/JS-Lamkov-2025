3470a4a8ecb82cd11712aa62a28d43b2f8545450
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-02-17 11:39:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const age = +prompt("how old are you?");
 
 switch (true) {
-  case age < 1:
-    {
-      console.log("Wrong age");
-    }
+  case age < 1: {
+    console.log("Wrong age");
     break;
+  }
 }
