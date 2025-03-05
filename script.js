e686f889ff3e20eed13c671352cf13be7e297e2d
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-03-05 12:09:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-console.log("loop now starting...");
+// console.log("loop now starting...");
 
-for (let i = 0; i < 10; i++) {
-  if (i % 2 === 0) {
-    continue;
-  }
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
 
-  console.log(i);
+//   console.log(i);
+// }
+
+// console.log(".... loop is over!");
+
+function logHello() {
+  console.log("Hello");
 }
 
-console.log(".... loop is over!");
+logHello();
