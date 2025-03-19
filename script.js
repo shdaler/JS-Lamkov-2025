9bdbfa514ff5904f47cd6508086aafb2bfe5e56a
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-03-19 12:23:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const user = {
-  name: "ALex",
+// const user = {
+//   name: "ALex",
+//   age: 28,
+// };
+
+// console.log("isDeveloper" in user);
+
+const nums = {
+  name: "Alex",
+  2: "Second",
+  1: "First",
+  3: "Third",
   age: 28,
 };
 
-console.log("isDeveloper" in user);
+for (const num in nums) {
+  console.log(nums[num]);
+}
+
+console.log(nums);
